93c047abe69e6ce0bc0b4823518b64ddac18919b
Olena Ageyeva <eageeva@hotmail.com>
2017-09-26 01:52:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
-while position_taken?(board,index)
+while !valid_move?(board,index)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
